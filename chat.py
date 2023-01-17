8ba383e36fe0bc362187f42acb36349c38aa1827
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2023-01-17 05:03:59 +0500

    fix: added emit function

--- chat.py
+++ chat.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,6 +11,12 @@ if __name__ == '__main__':
     socketio.run(app)
 
 
+@app.get('/')
+def index():
+    return "API working!"
+
+
 @socketio.on('message')
 def handle_message(data):
     print(data)
+    emit("chat", data, broadcast=True)
